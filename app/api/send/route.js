61d22e051b71e84ccf56eb722ab5c77ae7aa32cb
Charmie <charmiedubongco@gmail.com>
2024-07-26 13:31:52 -0700

    changed emailTemplate routing

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
-import { EmailTemplate } from "../../components/EmailTemplate";
+import EmailTemplate  from "../../components/EmailTemplate";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
